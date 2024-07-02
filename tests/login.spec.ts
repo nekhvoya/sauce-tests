b7346dbe4d57e36f1345d3ebb24df42e197e9eab
Yuliya Novikova <yuliya.novikova@postnl.nl>
2024-07-02 12:46:47 +0200

    test screenshots

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -12,7 +12,7 @@ test.describe('Login/logout', () => {
     test('Standard user is able to log in and logout', async ({ loginPage, inventoryPage, header }) => {  
         await loginPage.typeUsername(User.STANDARD_USER.username);
         await loginPage.typePassword(User.STANDARD_USER.password);
-        await loginPage.clickLoginButton();
+        // await loginPage.clickLoginButton();
         await inventoryPage.isOpened();
         await header.logout();
         await loginPage.isOpened();
