32fd37ff5195780386c218a023a0a8cf3f97b40c
Yuliya Novikova <yuliya.novikova@postnl.nl>
2024-07-02 20:08:33 +0200

    updated config for the api tests

--- api.playwright.config.ts
+++ api.playwright.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from '@playwright/test';
+
 export default defineConfig({
     testDir: './tests/api',
+    fullyParallel: true,
+    forbidOnly: !!process.env.CI,
+    retries: 0,
+    workers: process.env.CI ? 4 : undefined,
+    reporter: 'html',
     use: {
         baseURL: 'https://reqres.in/'
     }
